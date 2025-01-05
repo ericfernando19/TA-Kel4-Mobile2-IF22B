c1c64ac007d2acadbda9d8aec2f1793212742622
ramadhanriskiii45 <ramadhanriskiii45@gmail.com>
2025-01-05 23:15:51 +0000

    update ui kebijakan

--- lib/app/modules/profile/views/kebijakan.dart
+++ lib/app/modules/profile/views/kebijakan.dart
@@ -8,6 +8,7 @@ class KebijakanDanPrivasi extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Kebijakan dan Privasi'),
+        backgroundColor: Colors.blueAccent, // Warna app bar
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -15,53 +16,129 @@ class KebijakanDanPrivasi extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Judul utama
               Text(
                 'Kebijakan Privasi',
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
                 ),
               ),
+              SizedBox(height: 20),
+
+              // Paragraf kebijakan privasi
+              Text(
+                'Kami menghargai privasi Anda dan berkomitmen untuk melindungi informasi pribadi yang Anda berikan saat menggunakan aplikasi kami. Kebijakan ini menjelaskan jenis informasi yang kami kumpulkan, bagaimana kami menggunakannya, hak Anda sehubungan dengan data pribadi Anda, serta ketentuan penggunaan aplikasi.',
+                style: TextStyle(fontSize: 16, height: 1.6),
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(height: 20),
+
+              // Subjudul dan isi kebijakan
+              _buildSection(
+                title: '1. Informasi yang Kami Kumpulkan',
+                content:
+                    'Kami mengumpulkan informasi berikut saat Anda menggunakan aplikasi kami:\n'
+                    '- Data Pengguna: Nama, alamat email, dan informasi lainnya yang Anda berikan secara sukarela.\n'
+                    '- Data Penggunaan: Informasi tentang bagaimana Anda menggunakan aplikasi kami, termasuk interaksi dengan fitur penghitung protein.\n'
+                    '- Data Perangkat: Informasi perangkat yang digunakan untuk mengakses aplikasi, seperti tipe perangkat, sistem operasi, dan ID perangkat.',
+              ),
               SizedBox(height: 16),
-            Text(
-  'Kami menghargai privasi Anda dan berkomitmen untuk melindungi informasi pribadi yang Anda berikan saat menggunakan aplikasi kami. Kebijakan ini menjelaskan jenis informasi yang kami kumpulkan, bagaimana kami menggunakannya, hak Anda sehubungan dengan data pribadi Anda, serta ketentuan penggunaan aplikasi.\n\n'
-  '1. Informasi yang Kami Kumpulkan\n'
-  'Kami mengumpulkan informasi berikut saat Anda menggunakan aplikasi kami:\n'
-  '- Data Pengguna: Nama, alamat email, dan informasi lainnya yang Anda berikan secara sukarela.\n'
-  '- Data Penggunaan: Informasi tentang bagaimana Anda menggunakan aplikasi kami, termasuk interaksi dengan fitur penghitung protein.\n'
-  '- Data Perangkat: Informasi perangkat yang digunakan untuk mengakses aplikasi, seperti tipe perangkat, sistem operasi, dan ID perangkat.\n\n'
-  '2. Penggunaan Informasi\n'
-  'Kami menggunakan informasi yang kami kumpulkan untuk:\n'
-  '- Menyediakan dan meningkatkan fungsionalitas aplikasi penghitung protein.\n'
-  '- Menyesuaikan pengalaman pengguna dan memberikan rekomendasi yang relevan.\n'
-  '- Menghubungi Anda untuk memberi tahu pembaruan atau perubahan pada aplikasi.\n\n'
-  '3. Keamanan Data\n'
-  'Kami berkomitmen untuk melindungi data pribadi Anda dengan langkah-langkah keamanan yang sesuai, namun tidak ada metode pengiriman atau penyimpanan data yang sepenuhnya aman.\n\n'
-  '4. Berbagi Informasi\n'
-  'Kami tidak akan membagikan informasi pribadi Anda kepada pihak ketiga tanpa izin Anda, kecuali jika diwajibkan oleh hukum.\n\n'
-  '5. Hak Anda\n'
-  'Anda memiliki hak untuk mengakses, memperbarui, atau menghapus informasi pribadi yang kami miliki tentang Anda. Anda juga dapat menonaktifkan pengumpulan data jika diinginkan.\n\n'
-  '6. Penggunaan Aplikasi\n'
-  'Aplikasi ini hanya boleh digunakan untuk tujuan pribadi dan non-komersial. Anda setuju untuk tidak menggunakan aplikasi untuk tujuan yang melanggar hukum atau merugikan orang lain.\n\n'
-  '7. Akses ke Fitur\n'
-  'Kami memberikan akses terbatas dan tidak eksklusif kepada Anda untuk menggunakan aplikasi, tergantung pada jenis dan versi akun Anda.\n\n'
-  '8. Kewajiban Pengguna\n'
-  '- Anda bertanggung jawab atas informasi yang dimasukkan ke dalam aplikasi.\n'
-  '- Anda tidak boleh merusak, membobol, atau mengakses aplikasi dengan cara yang tidak sah.\n'
-  '- Anda setuju untuk tidak menyebarkan atau menggunakan informasi yang diperoleh dari aplikasi untuk tujuan yang tidak sah.\n\n'
-  '9. Pembatasan Tanggung Jawab\n'
-  'Kami tidak bertanggung jawab atas kerusakan atau kerugian yang timbul akibat penggunaan aplikasi, termasuk kesalahan dalam penghitungan protein yang disarankan. Penggunaan aplikasi sepenuhnya adalah risiko pengguna.\n\n'
-  '10. Perubahan pada Kebijakan\n'
-  'Kami berhak untuk mengubah kebijakan ini kapan saja tanpa pemberitahuan sebelumnya. Anda diharapkan untuk memeriksa kebijakan secara berkala untuk mengetahui pembaruan.',
-  style: TextStyle(fontSize: 16),
-  textAlign: TextAlign.justify,
-)
 
+              _buildSection(
+                title: '2. Penggunaan Informasi',
+                content:
+                    'Kami menggunakan informasi yang kami kumpulkan untuk:\n'
+                    '- Menyediakan dan meningkatkan fungsionalitas aplikasi penghitung protein.\n'
+                    '- Menyesuaikan pengalaman pengguna dan memberikan rekomendasi yang relevan.\n'
+                    '- Menghubungi Anda untuk memberi tahu pembaruan atau perubahan pada aplikasi.',
+              ),
+              SizedBox(height: 16),
 
+              _buildSection(
+                title: '3. Keamanan Data',
+                content:
+                    'Kami berkomitmen untuk melindungi data pribadi Anda dengan langkah-langkah keamanan yang sesuai, namun tidak ada metode pengiriman atau penyimpanan data yang sepenuhnya aman.',
+              ),
+              SizedBox(height: 16),
+
+              _buildSection(
+                title: '4. Berbagi Informasi',
+                content:
+                    'Kami tidak akan membagikan informasi pribadi Anda kepada pihak ketiga tanpa izin Anda, kecuali jika diwajibkan oleh hukum.',
+              ),
+              SizedBox(height: 16),
+
+              _buildSection(
+                title: '5. Hak Anda',
+                content:
+                    'Anda memiliki hak untuk mengakses, memperbarui, atau menghapus informasi pribadi yang kami miliki tentang Anda. Anda juga dapat menonaktifkan pengumpulan data jika diinginkan.',
+              ),
+              SizedBox(height: 16),
+
+              _buildSection(
+                title: '6. Penggunaan Aplikasi',
+                content:
+                    'Aplikasi ini hanya boleh digunakan untuk tujuan pribadi dan non-komersial. Anda setuju untuk tidak menggunakan aplikasi untuk tujuan yang melanggar hukum atau merugikan orang lain.',
+              ),
+              SizedBox(height: 16),
+
+              _buildSection(
+                title: '7. Akses ke Fitur',
+                content:
+                    'Kami memberikan akses terbatas dan tidak eksklusif kepada Anda untuk menggunakan aplikasi, tergantung pada jenis dan versi akun Anda.',
+              ),
+              SizedBox(height: 16),
+
+              _buildSection(
+                title: '8. Kewajiban Pengguna',
+                content:
+                    '- Anda bertanggung jawab atas informasi yang dimasukkan ke dalam aplikasi.\n'
+                    '- Anda tidak boleh merusak, membobol, atau mengakses aplikasi dengan cara yang tidak sah.\n'
+                    '- Anda setuju untuk tidak menyebarkan atau menggunakan informasi yang diperoleh dari aplikasi untuk tujuan yang tidak sah.',
+              ),
+              SizedBox(height: 16),
+
+              _buildSection(
+                title: '9. Pembatasan Tanggung Jawab',
+                content:
+                    'Kami tidak bertanggung jawab atas kerusakan atau kerugian yang timbul akibat penggunaan aplikasi, termasuk kesalahan dalam penghitungan protein yang disarankan. Penggunaan aplikasi sepenuhnya adalah risiko pengguna.',
+              ),
+              SizedBox(height: 16),
+
+              _buildSection(
+                title: '10. Perubahan pada Kebijakan',
+                content:
+                    'Kami berhak untuk mengubah kebijakan ini kapan saja tanpa pemberitahuan sebelumnya. Anda diharapkan untuk memeriksa kebijakan secara berkala untuk mengetahui pembaruan.',
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  // Fungsi untuk membuat bagian dengan subjudul dan isi kebijakan
+  Widget _buildSection({required String title, required String content}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
+        SizedBox(height: 8),
+        Text(
+          content,
+          style: TextStyle(fontSize: 16, height: 1.6),
+          textAlign: TextAlign.justify,
+        ),
+      ],
     );
   }
 }
