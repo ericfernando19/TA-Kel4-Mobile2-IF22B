29fc91763dd0c1f15f774814bb31473fd67b1834
ramadhanriskiii45 <ramadhanriskiii45@gmail.com>
2025-01-07 19:40:46 +0000

    Menambahkan deskripsi tentang aplikasi penghitung protein pada halaman AboutPage

--- lib/app/modules/profile/views/tentang_aplikasi.dart
+++ lib/app/modules/profile/views/tentang_aplikasi.dart
@@ -23,4 +23,22 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Tentang Aplikasi'),
       ),
-      
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Tentang Aplikasi Penghitung Protein',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Aplikasi ini dirancang untuk membantu pengguna menghitung kebutuhan protein harian mereka berdasarkan usia, jenis kelamin, dan tingkat aktivitas fisik.', 
+              
+              style: TextStyle(fontSize: 16, color: Colors.black87),
+            ),
